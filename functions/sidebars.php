7b3fe5b08a659428c1aecf57ce6f836147bf3ee0
Chris Schmitz <ccschmitz@gmail.com>
2011-12-29 22:24:54 -0600

    Removed sidebars from old project

--- functions/sidebars.php
+++ functions/sidebars.php
@@ -18,54 +18,10 @@ function cs_register_sidebars() {
 		'name' => __('Blog'),
 		'id' => 'blog',
 		'description' => __('This sidebar will be displayed on blog posts and other blog-related pages.'),
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-		'before_widget' => '<div class="widget">',
-		'after_widget' => '</div>'
-	));
-	
-	// About sidebar
-	register_sidebar(array(
-		'name' => __('About'),
-		'id' => 'about',
-		'description' => __('This sidebar will be displayed on the about page.'),
 		'before_title' => '<h2>',
 		'after_title' => '</h2>',
 		'before_widget' => '<div class="widget">',
 		'after_widget' => '</div>'
 	));
 	
-	// Product sidebar
-	register_sidebar(array(
-		'name' => __('Product'),
-		'id' => 'product',
-		'description' => __('This sidebar will be displayed on the product page.'),
-		'before_title' => '<h2>',
-		'after_title' => '</h2>',
-		'before_widget' => '<div class="widget">',
-		'after_widget' => '</div>'
-	));
-
-	// Contact sidebar
-	register_sidebar(array(
-		'name' => __('Contact'),
-		'id' => 'contact',
-		'description' => __('This sidebar will be displayed on the contact page.'),
-		'before_title' => '<h2>',
-		'after_title' => '</h2>',
-		'before_widget' => '<div class="widget">',
-		'after_widget' => '</div>'
-	));
-	
-	// Integrations sidebar
-	register_sidebar(array(
-		'name' => __('Integrations'),
-		'id' => 'integrations',
-		'description' => __('This sidebar will be displayed on the integrations page.'),
-		'before_title' => '<h2>',
-		'after_title' => '</h2>',
-		'before_widget' => '<div class="widget">',
-		'after_widget' => '</div>'
-	));
-
 }
