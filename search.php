4fdee1d505add3976ddb1bbd08d8c221dffe50eb
Chris Schmitz <ccschmitz@gmail.com>
2011-12-29 20:52:15 -0600

    Fixed error in search form and cleaned up formatting

--- search.php
+++ search.php
@@ -1,21 +1,21 @@
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : ?>
+	<?php if ( have_posts() ) : ?>
 		
-		<h1>Search results for &ldquo;<?php echo $s; ?>&rdquo;</h1>
+		<h2>Search results for &ldquo;<?php echo $s; ?>&rdquo;</h2>
 
-		<?php while (have_posts()) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 		
 			<?php get_template_part( 'loop', 'feed' ); ?>
 
 		<?php endwhile; ?>
 		
-		<?php if (show_posts_nav()) : ?>
+		<?php if ( show_posts_nav() ) : ?>
 		
 		<nav>
 			<ul>
-				<li><?php next_posts_link('&laquo; Previous page') ?></li>
-				<li><?php previous_posts_link('Next page &raquo;') ?></li>
+				<li><?php next_posts_link( '&laquo; Previous page' ); ?></li>
+				<li><?php previous_posts_link( 'Next page &raquo;' ); ?></li>
 			</ul>
 		</nav>
 		
@@ -23,7 +23,7 @@
 		
 	<?php else : ?>
 		
-		<h1>Nothing found for &ldquo;<?php echo $s; ?>&rdquo;</h1>
+		<h2>Nothing found for &ldquo;<?php echo $s; ?>&rdquo;</h2>
 
 		<?php get_search_form(); ?>
 		
